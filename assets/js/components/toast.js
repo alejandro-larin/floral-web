39ca48769ad2d8fc95dc63e5aef6f0c039d611bb
Alex!? 🍒 <alejandro.larin.orellana@gmail.com>
2025-11-17 02:36:12 +0100

    feat(component): add new component toast

---
+++ assets/js/components/toast.js
@@ -0,0 +1,19 @@
+export function showErrorToast(message, duration = 3000) {
+    const container = document.getElementById('toast-container');
+
+    const toast = document.createElement('div');
+    toast.className = 'toast';
+    toast.textContent = message;
+
+    container.appendChild(toast);
+
+
+    setTimeout(() => {
+        toast.classList.add('show');
+    }, 100); 
+
+    setTimeout(() => {
+        toast.classList.remove('show');
+        setTimeout(() => container.removeChild(toast), 500); // espera a la transición
+    }, duration);
+}
