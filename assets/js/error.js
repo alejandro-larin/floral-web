b3729f3c67cd0238bdd6f76ec3a9bf74c2de8106
Alex!? 🍒 <alejandro.larin.orellana@gmail.com>
2025-11-17 02:35:51 +0100

    feat(move): move files and create new dirs how private, auth , lib and components

--- assets/js/error.js
+++
@@ -1,7 +0,0 @@
-export class ValidateDataError extends Error {
-    constructor(errors) {
-        super("There are errors in validation");
-        this.name = "ValidateDataError";
-        this.errors = errors;    
-    }
-}
